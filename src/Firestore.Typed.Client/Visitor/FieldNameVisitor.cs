38db23e6c7ded801bfa4a40289562845aff466b4
Mihail Brinza <brinza@deon.de>
2026-03-08 13:52:40 +0000

    Simplify FieldNameVisitor and bump version to 2.0.1
    Replace manual string prepending with List<string> segments joined at the end,
    and use GetCustomAttribute<T>() instead of scanning all attributes with LINQ.
    Bump package version to 2.0.1.

--- src/Firestore.Typed.Client/Visitor/FieldNameVisitor.cs
+++ src/Firestore.Typed.Client/Visitor/FieldNameVisitor.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -9,40 +9,24 @@ namespace Firestore.Typed.Client.Visitor
 {
     public class FieldNameVisitor : ExpressionVisitor
     {
-        private const char FieldSeparator = '.';
-        private static readonly Type FirestorePropertyAttribute = typeof(FirestorePropertyAttribute);
-        public string FieldName { get; private set; } = string.Empty;
+        private readonly List<string> _segments = new();
+
+        public string FieldName => string.Join(".", Enumerable.Reverse(_segments));
 
         protected override Expression VisitMember(MemberExpression node)
         {
-            if (!string.IsNullOrEmpty(FieldName))
+            FirestorePropertyAttribute? firestoreAttribute = node.Member.GetCustomAttribute<FirestorePropertyAttribute>();
+
+            if (firestoreAttribute is not null && !string.IsNullOrEmpty(firestoreAttribute.Name))
             {
-                AddToFieldName(FieldSeparator);
+                _segments.Add(firestoreAttribute.Name);
+            }
+            else
+            {
+                _segments.Add(node.Member.Name);
             }
 
-            Attribute? propAttribute = node.Member
-                .GetCustomAttributes()
-                .FirstOrDefault(attribute => attribute.GetType() == FirestorePropertyAttribute);
-
-            if (propAttribute is FirestorePropertyAttribute firestoreAttribute
-                && !string.IsNullOrEmpty(firestoreAttribute.Name))
-            {
-                AddToFieldName(firestoreAttribute.Name);
-                return base.VisitMember(node);
-            }
-
-            AddToFieldName(node.Member.Name);
             return base.VisitMember(node);
-        }
-
-        private void AddToFieldName(string field)
-        {
-            FieldName = field + FieldName;
-        }
-
-        private void AddToFieldName(char character)
-        {
-            FieldName = character + FieldName;
         }
     }
 }
